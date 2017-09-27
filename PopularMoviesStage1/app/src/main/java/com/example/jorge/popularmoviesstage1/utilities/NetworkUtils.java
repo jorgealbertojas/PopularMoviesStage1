f62edb6c4963edf9ee5c4ade0a5c6c26adb73d5e
jorgealbertojas <jorgealbertojas@gmail.com>
2017-09-27 19:01:45 -0300

    feat: AsyncTask.
    Access json using AsyncTask.

---
+++ PopularMoviesStage1/app/src/main/java/com/example/jorge/popularmoviesstage1/utilities/NetworkUtils.java
@@ -0,0 +1,81 @@
+package com.example.jorge.popularmoviesstage1.utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+import static com.example.jorge.popularmoviesstage1.utilities.information.API_KEY;
+import static com.example.jorge.popularmoviesstage1.utilities.information.MIDLE;
+
+/**
+ * Created by jorge on 27/09/2017.
+ */
+
+public class NetworkUtils {
+    final static String GITHUB_BASE_URL =
+            "https://api.themoviedb.org/3/";
+
+
+
+    final static String PARAM_QUERY = "q";
+
+    /*
+     * The sort field. One of stars, forks, or updated.
+     * Default: results are sorted by best match if no field is specified.
+     */
+    final static String PARAM_SORT = "sort";
+    final static String sortBy = "stars";
+
+    /**
+     * Builds the URL used to query GitHub.
+     *
+     * @param githubSearchQuery The keyword that will be queried for.
+     * @return The URL to use to query the GitHub.
+     */
+    public static URL buildUrl(String githubSearchQuery) {
+        Uri builtUri = Uri.parse(GITHUB_BASE_URL +  MIDLE + API_KEY ).buildUpon()
+                //.appendQueryParameter(PARAM_QUERY, githubSearchQuery)
+                //.appendQueryParameter(PARAM_SORT, sortBy)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
---
+++ PopularMoviesStage1/app/src/main/java/com/example/jorge/popularmoviesstage1/utilities/information.java
@@ -0,0 +1,14 @@
+package com.example.jorge.popularmoviesstage1.utilities;
+
+/**
+ * Created by jorge on 27/09/2017.
+ */
+
+public class information {
+
+    //public final static String API_KEY = "?api_key=24115bedf3566e17b46753fb8f29cbff";
+    public final static String API_KEY = "";
+
+
+    public final static String MIDLE = "movie/popular";
+}
